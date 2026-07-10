e6efbac049df59787b0ea8526cb9cef84fa7fbf2
aaronsry9 <aaronsry9@gmail.com>
2026-07-10 15:57:22 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-// ===== 你的個人設定（只需填一次，之後更新 App 不用再動這個檔案）=====
+// ===== 只需填 API_URL 一個東西，TOKEN 已經設好不要動 =====
 const CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbw1V369px40mytShin3PjfxAlXNUj1UN7vHPEsKsUtWfYgefpW__fifgJ0WG1MJIXZf/exec",
-  TOKEN: "Aaron" // 需與 Code.gs 裡的 TOKEN 一致
+  API_URL: "https://script.google.com/macros/s/AKfycbzgQ-sokP8LMN516JJj2K1ywaSqIK0EixJzUd13fFiZBGKfwBXtfV28X-BoZjJjy4Hy/exec",
+  TOKEN: "AaronRun2026"
 };
