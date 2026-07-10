a2610b88ab52e9a56e64db34fcaea737664f8945
aaronsry9 <aaronsry9@gmail.com>
2026-07-10 19:30:23 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // ===== 只需填 API_URL 一個東西，TOKEN 已經設好不要動 =====
 const CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbzgQ-sokP8LMN516JJj2K1ywaSqIK0EixJzUd13fFiZBGKfwBXtfV28X-BoZjJjy4Hy/exec",
+  API_URL: "https://script.google.com/macros/s/AKfycbwDIgThaPwJiquKUEkBr0F7urLFPGA5yDrgETmiYKB45440a2wW-xktkakl6PGoOWFgfg/exec",
   TOKEN: "AaronRun2026"
 };
